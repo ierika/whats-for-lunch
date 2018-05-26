863bf0b3148265c1a64a64dcf97d32e4738a1fae
Erika <haloerika@gmail.com>
2018-05-26 18:48:18 +0900

    use env port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const dotenv = require('dotenv');
 dotenv.load('./env');
 
 // Constants
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DEBUG = process.env.DEBUG || false;
 const BASE_DIR = __dirname;
 
